10dfcdf0be9e36e2cc5012deaca411eea41b64ea
LKing <1599863094@qq.com>
2016-08-15 16:44:18 +0800

    整理授权登录返回值

--- app/src/main/java/com/hq/other/otherdemo/OtherLoginActivity.java
+++ app/src/main/java/com/hq/other/otherdemo/OtherLoginActivity.java
@@ -3,6 +3,7 @@ package com.hq.other.otherdemo;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
@@ -11,17 +12,18 @@ import android.widget.Toast;
 import com.hqjy.opensdk.opensdk.Statistics;
 public class OtherLoginActivity extends Activity {
 	TextView txt_back;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.other_login_activity);
-		Statistics.getInstance().getSP(OtherLoginActivity.this,"");
+		Statistics.getInstance().getSP(OtherLoginActivity.this, "");
 		txt_back = (TextView) findViewById(R.id.other_youe_back);
 		findViewById(R.id.other_btn).setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Statistics.getInstance().getSP(OtherLoginActivity.this,"you");
-				Intent intent = new Intent(OtherLoginActivity.this,MainActivity.class);
+				Statistics.getInstance().getSP(OtherLoginActivity.this, "you");
+				Intent intent = new Intent(OtherLoginActivity.this, MainActivity.class);
 				startActivity(intent);
 				finish();
 			}
@@ -30,7 +32,7 @@ public class OtherLoginActivity extends Activity {
 		findViewById(R.id.other_btn_youe).setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Statistics.getInstance().init("appid=001",OtherLoginActivity.this);// 初始化SDK，判断是否安装了优e学堂，并跳转到不同的界面
+				Statistics.getInstance().init("appid=001", OtherLoginActivity.this);// 初始化SDK，判断是否安装了优e学堂，并跳转到不同的界面
 			}
 		});
 
@@ -38,26 +40,39 @@ public class OtherLoginActivity extends Activity {
 
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		Statistics.getInstance().getData(requestCode,resultCode,data,OtherLoginActivity.this);
-		if(null!=Statistics.getInstance().getData(requestCode,resultCode,data,OtherLoginActivity.this)){
-			if(Statistics.getInstance().getData(requestCode,resultCode,data,OtherLoginActivity.this).equals("安装未登录网络请求----->返回JSON数据")){
-				Intent intent = new Intent(OtherLoginActivity.this,MainActivity.class);
-				intent.putExtra("true", "手动授权成功，\n账号和优e学堂登录账号不同.\n安装未登录----->返回JSON数据");
-				startActivity(intent);
-				finish();
-			}else
-			if(Statistics.getInstance().getData(requestCode,resultCode,data,OtherLoginActivity.this).equals("安装已登录网络请求----->返回JSON数据")){
-				Intent intent = new Intent(OtherLoginActivity.this,MainActivity.class);
-				intent.putExtra("true", "自动授权成功，\n账号和优e学堂登录账号相同.\n安装已登录----->返回JSON数据");
-				startActivity(intent);
-				finish();
-			}
-			else{
+		Statistics.getInstance().getData(requestCode, resultCode, data, OtherLoginActivity.this);
+		switch (requestCode) {
+			case 1:
+				if (resultCode == Activity.RESULT_OK) {
+					Intent intent = new Intent(OtherLoginActivity.this, MainActivity.class);
+					intent.putExtra("true", "手动授权成功，\n第三方安装未登录----->返回数据:" + data.getStringExtra("user"));
+					startActivity(intent);
+					finish();
+				}
+				break;
+			case 2:
+				if (resultCode == Activity.RESULT_OK) {
+					Intent intent = new Intent(OtherLoginActivity.this, MainActivity.class);
+					intent.putExtra("true", "自动授权成功，\n第三方安装已登录----->返回数据:" + data.getStringExtra("user"));
+					startActivity(intent);
+					finish();
+
+				}
+				break;
+			default:
 				Toast.makeText(getApplicationContext(), "授权失败", Toast.LENGTH_SHORT).show();
-			}
+				break;
 		}
+	}
 
 
-
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) { //按下的如果是BACK，同时没有重复
+			//退出应用
+			finish();
+			return true;
+		}
+		return super.onKeyDown(keyCode, event);
 	}
 }

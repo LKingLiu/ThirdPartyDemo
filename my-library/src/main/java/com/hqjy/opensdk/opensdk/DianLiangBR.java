8bc3895c1e36ff26c1df4c163cdc63cf405144a4
LKing <1599863094@qq.com>
2016-08-15 16:46:50 +0800

    切换账号时，第三方和优e两者账号的对比修改

--- my-library/src/main/java/com/hqjy/opensdk/opensdk/DianLiangBR.java
+++ my-library/src/main/java/com/hqjy/opensdk/opensdk/DianLiangBR.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.text.TextUtils;
 import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by annuo on 2015/5/16.
@@ -22,20 +23,12 @@ public class DianLiangBR extends BroadcastReceiver {
          String name = intent.getStringExtra("name");
          int number = intent.getIntExtra("number", 0);
          if (null != name) {
-            Log.e("info", "弹出对话框");
-
             SharedPreferences sp = context.getSharedPreferences("data", Context.MODE_PRIVATE);
             String data = sp.getString("name", null);
             Log.e("info", "name : " + name + "---- data : " + data);
             if (name.equals(data) || TextUtils.isEmpty(data)) {
-               Log.e("info", "第三方  ==优e");
-               // Uri uri =
-               // Uri.parse("market://search?q=pname:com.example.otherdemo");
-               // Intent it = new Intent(Intent.ACTION_VIEW, uri);
-               // it.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-               // context.startActivity(it);
+               Log.e("info", "第三方  ==  优e");
             } else {
-
                Intent in = new Intent(context, DialogActivity.class);
                in.putExtra("name", name);
                in.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);// 广播中启动Activity需要设置此flag，因为Activity需要一个栈
